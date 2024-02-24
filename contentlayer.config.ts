9374f1208ea4a4dd3db69b885c7a6a8914efa4e9
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-25 05:16:21 +0900

    [feat] 코드 블럭 타이틀 추가 및 스타일링

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,7 +1,7 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
-import { highlight } from 'rehype-highlight';
+import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrettyCode from 'rehype-pretty-code';
-
+import remarkGfm from 'remark-gfm';
 
 const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -12,25 +12,22 @@ const Post = defineDocumentType(() => ({
     date: { type: 'date', required: true },
     category: { type: 'string', required: true },
     description: { type: 'string', required: true },
-    thumbnail: {type: 'string', required: true},
+    thumbnail: { type: 'string', required: true },
   },
 }));
+
+const rehypeOptions = {
+  theme: 'github-dark',
+  keepBackground: true,
+};
 
 const contentSource = makeSource({
   // 마크다운 파일이 저장되어 있는 루트 폴더
   contentDirPath: 'posts',
   documentTypes: [Post],
   mdx: {
-    remarkPlugins: [],
-    rehypePlugins: [
-      [
-        rehypePrettyCode,
-        {
-          theme: 'github-dark', // 코드작성시 적용할 테마
-        },
-      ],
-      highlight,
-    ],
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeCodeTitles, [rehypePrettyCode, rehypeOptions]],
   },
 });
 
