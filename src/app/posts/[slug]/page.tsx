0738281c404cd0af5a96621a67e3b7f26bd46749
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-25 05:49:06 +0900

    [UI] 게시물 상세

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { allPosts } from 'contentlayer/generated';
+import { format } from 'date-fns';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import Image from 'next/image';
 
+// 정적 경로 생성 (getStaticPath)
 export const generateStaticParams = () => {
   return allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 };
@@ -10,9 +13,29 @@ export default function Posts({ params }) {
   const MDXComponent = useMDXComponent(post.body.code);
 
   return (
-    <div className='mt-10 prose'>
-      <h1 className='text-sky-700'>{post.title}</h1>
-      <MDXComponent />
+    <div className='flex flex-col pb-20 gap-7 mx-5'>
+      <h1 className='text-5xl font-bold mb-1'>{post.title}</h1>
+      <div className='flex flex-row items-center justify-between -mb-2'>
+        <div className='rounded-md bg-slate-800 px-2 py-1 text-md text-white'>
+          {post.category}
+        </div>
+        <div className='text-md'>
+          {format(new Date(post.date), 'yyyy년 M월 d일')}
+        </div>
+      </div>
+      <Image
+          src={post.thumbnail}
+          alt={post.title}
+          width={0}
+          height={0}
+          sizes='100vw'
+          className='h-auto w-full mb-8'
+        />
+
+      <div className='prose'>
+        <MDXComponent className='min-w-full' />
+      </div>
+
     </div>
   );
 }
