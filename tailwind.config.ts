503a72bbc1f8d45522aad036f277956d87c09cfc
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-25 05:16:59 +0900

    [UI] 마크다운 스타일링 설정 및 인라인 코드블럭 오류 수정
    인라인 코드 블럭에서 백틱이 없어지지 않는 현상 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            fontSize: '1.125rem',
+            maxWidth: 'none',
+            h1: {
+              fontSize: '2rem',
+            },
+            code: {
+              '&::before': {
+                content: '"" !important',
+              },
+              '&::after': {
+                content: '"" !important',
+              },
+              backgroundColor: theme('colors.gray.100'),
+              color: theme('colors.red.500'),
+              fontWeight: '400',
+              padding: theme('spacing.1'),
+              borderRadius: theme('borderRadius.DEFAULT'),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
