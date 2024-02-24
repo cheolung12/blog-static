f1fa7760608cf5b4cd33abab885e6614c8bcbef2
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-25 08:18:17 +0900

    [UI] 시그니처 색상 지정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,17 @@ import ScrollProgressBar from './ScrollProgressBar';
 const Header: FC = () => {
   return (
     <div className='fixed w-full z-99'>
-    <header
-      className={`flex h-16 py-2 mx-auto w-[900px] bg-white items-center justify-between lg:h-20`}
-    >
-      <Link href='/'>
-        <h1 className='text-3xl font-bold lg:text-3xl '>cheolung.dev</h1>
-      </Link>
-      <Nav />
-    </header>
-    <ScrollProgressBar />
+      <header
+        className={`flex h-16 py-2 mx-auto w-[900px] bg-white items-center justify-between lg:h-20`}
+      >
+        <Link href='/'>
+          <h1 className='text-3xl font-bold lg:text-3xl '>
+            cheolung.<span className='text-signature'>dev</span>
+          </h1>
+        </Link>
+        <Nav />
+      </header>
+      <ScrollProgressBar />
     </div>
   );
 };
