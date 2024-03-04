3f1e68b325a467386dcc4e25f8ffda1c6874c686
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-03-04 11:52:32 +0900

    [UI] favicon 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'cheolung\'s blog',
   description: '개발 관련 이야기를 나누는 블로그입니다.',
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
